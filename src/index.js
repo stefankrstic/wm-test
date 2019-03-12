e9b5cefe610fdfc1ce29a007dd6c279a0cd68553
Stefan <stefan.k.krstic@gmail.com>
2019-03-12 18:11:47 +0100

    install normalize.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
+import 'normalize.css/normalize.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
